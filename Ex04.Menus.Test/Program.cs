53e2bb99c99d82aa0a45483b5faa4eb6e08a2a60
Yonty <jonathanpo@mta.ac.il>
2024-07-08 10:28:27 +0300

    yonti init

--- Ex04.Menus.Test/Program.cs
+++ Ex04.Menus.Test/Program.cs
@@ -1,17 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ex04.Menus.Test
-{
-    internal class Program
-    {
-        public static void Main()
-        {
-            Console.WriteLine("Just a test");
-            Console.ReadLine();
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
